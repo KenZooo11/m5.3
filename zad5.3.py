176329a61164e401a88b8dadee212ae7f31c6ae6
Kamil Toporek <kamil.toporek98@gmail.com>
2024-04-04 14:36:54 +0200

    Add function instance

--- zad5.3.py
+++ zad5.3.py
@@ -20,4 +20,22 @@ for _ in range(5):
 
 for card in business_cards:
     print(f"{card.first_name} {card.last_name}: {card.email}")
-    dsadas
+
+from faker import Faker
+
+fake = Faker()
+
+def generate_random_business_card():
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    company = fake.company()
+    position = fake.job()
+    email = fake.email()
+    return BusinessCard(first_name, last_name, company, position, email)
+
+random_card = generate_random_business_card()
+print(f"Imię: {random_card.first_name}")
+print(f"Nazwisko: {random_card.last_name}")
+print(f"Firma: {random_card.company}")
+print(f"Stanowisko: {random_card.position}")
+print(f"Email: {random_card.email}")
